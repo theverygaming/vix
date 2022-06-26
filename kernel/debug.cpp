6a32c8c7f06b370f3f7e5ecc4bcd99a8c83f901b
theverygaming <clemibunge@gmail.com>
2022-06-27 00:18:27 +0200

    :man_facepalming:

--- kernel/debug.cpp
+++ kernel/debug.cpp
@@ -6,7 +6,7 @@ void debug::stack_trace(uint32_t maxLength, uint32_t ebp) {
     printf("Stack trace:\n");
     for(uint32_t i = 0; ebp && i < maxLength; i++)
     {
-        if(!paging::is_readable((void*)ebp) && !paging::is_readable((void*)ebp + 4)) { break; }
+        if(!paging::is_readable((void*)ebp) || !paging::is_readable((void*)ebp + 4)) { break; }
         uint32_t eip = *((uint32_t*)ebp + 4);
         printf("->0x%p\n", eip);
         ebp = *((uint32_t*)ebp);
