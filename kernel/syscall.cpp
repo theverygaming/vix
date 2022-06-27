7cc6ba27dcfe5d26a6e93da634c15eedfe63e8e3
theverygaming <clemibunge@gmail.com>
2022-06-28 01:46:35 +0200

    working sys_read

--- kernel/syscall.cpp
+++ kernel/syscall.cpp
@@ -6,7 +6,8 @@
 #include "../config.h"
 
 void syscall::syscallHandler(isr::Registers* regs) {
-    multitasking::process* currentProcess = multitasking::getCurrentProcess();
+    multitasking::process* currentProcess = multitasking::getCurrentProcess(); // never use this to modify registers, it will not work
+    multitasking::context *current_context = (multitasking::context*)(KERNEL_VIRT_ADDRESS + REGISTER_STORE_OFFSET); // return values using this
 
     if(regs->eax == 1) { // sys_exit
         printf("syscall: sys_exit code: %d\n", regs->ebx);
@@ -22,29 +23,36 @@ void syscall::syscallHandler(isr::Registers* regs) {
     else if(regs->eax == 3) { // sys_read
         printf("syscall: sys_read\n");
 
-        multitasking::context *current_context = (multitasking::context*)(KERNEL_VIRT_ADDRESS + REGISTER_STORE_OFFSET);
         multitasking::process tempProcessStore;
         memcpy((char*)&tempProcessStore, (char*)currentProcess, sizeof(multitasking::process));
         multitasking::context tempContextStore;
         memcpy((char*)&tempContextStore, (char*)current_context, sizeof(multitasking::context));
 
+        int bufStart = drivers::keyboard::bufferlocation;
         asm("sti");
-        while(drivers::keyboard::bufferlocation < 10) {
-            
+        while(drivers::keyboard::bufferlocation - bufStart < regs->edx) {
+            if(drivers::keyboard::buffer[drivers::keyboard::bufferlocation] == '\n') {
+                break;
+            }
         }
         asm("cli");
+        uint32_t readCharacters = drivers::keyboard::bufferlocation - bufStart;
 
         drivers::keyboard::bufferlocation = -1;
         memcpy((char*)currentProcess, (char*)&tempProcessStore, sizeof(multitasking::process));
         memcpy((char*)current_context, (char*)&tempContextStore, sizeof(multitasking::context));
 
-        char string[11] = "/bin/bash\n";
-        memcpy((char*)regs->ecx, string, 10);
-        current_context->eax = 10;
+        memcpy((char*)regs->ecx, &drivers::keyboard::buffer[bufStart+1], readCharacters);
+        current_context->eax = readCharacters;
     }
     else if(regs->eax == 2) { // sys_fork
         printf("syscall: sys_fork\n");
         multitasking::fork_process(currentProcess);
+        current_context->eax = 0; // make the process think it's the child
+    }
+    else if(regs->eax == 11) { // sys_fork
+        printf("syscall: sys_execve\n");
+        printf("%s\n", regs->ebx);
     }
     else {
         printf("Unknown syscall %u\n", regs->eax);
