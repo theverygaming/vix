9370d6e8d039f90c9f3db8f6c0537423f6f929de
theverygaming <clemibunge@gmail.com>
2022-06-30 02:07:42 +0200

    simp time

--- kernel/stdio.h
+++ kernel/stdio.h
@@ -4,5 +4,6 @@
 void clrscr();
 void putc(char c);
 void puts(const char* str);
+void putcolor(int x, int y, uint8_t color);
 void printf(const char* fmt, ...);
 void print_buffer(const char* msg, const void* buffer, uint32_t count);
