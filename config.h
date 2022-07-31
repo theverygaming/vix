900b126cf2bc352266a776ccedcd9bcb1507e998
theverygaming <clemibunge@gmail.com>
2022-07-31 02:23:26 +0200

    more fixes

--- config.h
+++ config.h
@@ -5,17 +5,17 @@
 
 // directory size: 4096 bytes(ends 0x1001000)
 // tables size: (1024 * 1024 * 4) bytes
-#define PAGE_DIRECTORY_OFFSET 0x100000
+#define PAGE_DIRECTORY_OFFSET 0x200000
 #define PAGE_TABLES_OFFSET PAGE_DIRECTORY_OFFSET + (1024 * 4)
 
 // location where ISR stores registers, size: 32 bytes 
 //could be moved a bit
-#define REGISTER_STORE_OFFSET 0x502000
+#define REGISTER_STORE_OFFSET 0x602000
 
-#define GDT_OFFSET 0x502020
+#define GDT_OFFSET 0x602020
 
 // 80 * 25 * 2 = 4000 bytes, will be 4096 bytes bc it's paged
-#define VIDMEM_OFFSET 0x503000
+#define VIDMEM_OFFSET 0x603000
 
 #define KERNEL_START_STACK_POINTER_OFFSET 0xE4F000 // stack grows downwards
 #define KERNEL_ISR_STACK_POINTER_OFFSET 0xF49000
