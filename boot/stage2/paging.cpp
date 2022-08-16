9f551cd1bd25455dda0c60cc9bdf897ff023dc75
theverygaming <clemibunge@gmail.com>
2022-08-16 17:22:49 +0200

    paging redone

--- boot/stage2/paging.cpp
+++ boot/stage2/paging.cpp
@@ -9,25 +9,66 @@ uint32_t *page_directory = (uint32_t *)(KERNEL_PHYS_ADDRESS + PAGE_DIRECTORY_OFF
 extern "C" void loadPageDirectory(uint32_t *address);
 extern "C" void enablePaging();
 
+enum page_size { FOUR_KiB, FOUR_MB };
+enum page_priv { SUPERVISOR, USER };
+enum page_perms { R, RW };
+
+struct directoryEntry {
+    void *address;
+    enum page_size pagesize;
+    bool cache_disabled;
+    bool write_through;
+    enum page_priv priv;
+    enum page_perms perms;
+    bool present;
+};
+
+struct pagetableEntry {
+    void *address;
+    bool global;
+    bool cache_disabled;
+    bool write_through;
+    enum page_priv priv;
+    enum page_perms perms;
+    bool present;
+};
+
+uint32_t make_directory_entry(struct directoryEntry de) {
+    uint32_t entry = (uint32_t)de.address & 0xFFFFF000;
+    entry |= de.pagesize << 7;
+    entry |= de.cache_disabled << 4;
+    entry |= de.write_through << 3;
+    entry |= de.priv << 2;
+    entry |= de.perms << 1;
+    entry |= de.present;
+    return entry;
+}
+
+uint32_t make_table_entry(struct pagetableEntry pe) {
+    uint32_t entry = ((uint32_t)pe.address) & 0xFFFFF000;
+    entry |= pe.global << 8;
+    entry |= pe.cache_disabled << 6;
+    entry |= pe.write_through << 3;
+    entry |= pe.priv << 2;
+    entry |= pe.perms << 1;
+    entry |= pe.present;
+    return entry;
+}
+
 void *paging::get_physaddr(void *virtualaddr) {
     unsigned long pdindex = (unsigned long)virtualaddr >> 22;
     unsigned long ptindex = (unsigned long)virtualaddr >> 12 & 0x03FF;
 
     // TODO: check if entry is actually present
-    unsigned long *pd = (unsigned long *)page_directory;
-
-    unsigned long *pt = ((unsigned long *)pagetables) + (0x400 * pdindex);
-
-    return (void *)((pt[ptindex] & ~0xFFF) + ((unsigned long)virtualaddr & 0xFFF));
+    return (void *)(pagetables[pdindex][ptindex] & 0xFFFFF000);
 }
 
 // Both addresses have to be page-aligned!
 void map_page(void *physaddr, void *virtualaddr) {
-    unsigned long pDirIndex = (unsigned long)virtualaddr >> 22;
-    unsigned long pTableIndex = (unsigned long)virtualaddr >> 12 & 0x03FF;
+    uint32_t pDirIndex = (uint32_t)virtualaddr >> 22;
+    uint32_t pTableIndex = (uint32_t)virtualaddr >> 12 & 0x03FF;
 
-    paging::create_pagetable_entry(pDirIndex, pTableIndex, physaddr, false, false, false, paging::SUPERVISOR, paging::RW, true);
-    paging::create_directory_entry(pDirIndex, (void *)pagetables[pDirIndex], paging::FOUR_KiB, false, false, paging::SUPERVISOR, paging::RW, true);
+    pagetables[pDirIndex][pTableIndex] = make_table_entry({.address = physaddr, .global = false, .cache_disabled = false, .write_through = false, .priv = SUPERVISOR, .perms = RW, .present = true});
 }
 
 void stage2_pagetablefill() {
@@ -39,9 +80,10 @@ void stage2_pagetablefill() {
 void paging::initpaging() {
     for (int i = 0; i < 1024; i++) {
         for (unsigned int j = 0; j < 1024; j++) {
-            create_pagetable_entry(i, j, (void *)0, false, false, false, SUPERVISOR, RW, false);
+            pagetables[i][j] = 0;
         }
-        paging::create_directory_entry(i, (void *)pagetables[i], paging::FOUR_KiB, false, false, paging::SUPERVISOR, paging::RW, true);
+        page_directory[i] =
+            make_directory_entry({.address = (void *)pagetables[i], .pagesize = FOUR_KiB, .cache_disabled = false, .write_through = false, .priv = SUPERVISOR, .perms = RW, .present = true});
     }
 
     stage2_pagetablefill();
@@ -54,30 +96,4 @@ void paging::initpaging() {
     enablePaging();
     pagetables = (uint32_t(*)[1024])(KERNEL_VIRT_ADDRESS + PAGE_TABLES_OFFSET);
     page_directory = (uint32_t *)(KERNEL_VIRT_ADDRESS + PAGE_DIRECTORY_OFFSET);
-}
-
-void paging::create_pagetable_entry(int tablenum, int entrynum, void *address, bool global, bool cache_disabled, bool write_through, enum page_priv priv, enum page_perms perms, bool present) {
-    uint32_t tentry = (uint32_t)address;
-    tentry |= global << 8;
-    tentry |= cache_disabled << 6;
-    tentry |= write_through << 3;
-    tentry |= priv << 2;
-    tentry |= perms << 1;
-    tentry |= present;
-    pagetables[tablenum][entrynum] = tentry;
-}
-
-void paging::delete_pagetable_entry(int tablenum, int entrynum) {
-    create_pagetable_entry(tablenum, entrynum, 0, false, true, false, SUPERVISOR, RW, false);
-}
-
-void paging::create_directory_entry(int tablenum, void *address, enum page_size pagesize, bool cache_disabled, bool write_through, enum page_priv priv, enum page_perms perms, bool present) {
-    uint32_t direntry = (uint32_t)address;
-    direntry |= pagesize << 7;
-    direntry |= cache_disabled << 4;
-    direntry |= write_through << 3;
-    direntry |= priv << 2;
-    direntry |= perms << 1;
-    direntry |= present;
-    page_directory[tablenum] = direntry;
 }
--- kernel/arch/x86/paging.cpp
+++ kernel/arch/x86/paging.cpp
@@ -14,64 +14,75 @@ enum page_size { FOUR_KiB, FOUR_MB };
 enum page_priv { SUPERVISOR, USER };
 enum page_perms { R, RW };
 
-void create_directory_entry(int tablenum, void *address, enum page_size pagesize, bool cache_disabled, bool write_through, enum page_priv priv, enum page_perms perms, bool present) {
-    uint32_t direntry = (uint32_t)address;
-    direntry |= pagesize << 7;
-    direntry |= cache_disabled << 4;
-    direntry |= write_through << 3;
-    direntry |= priv << 2;
-    direntry |= perms << 1;
-    direntry |= present;
-    page_directory[tablenum] = direntry;
+struct directoryEntry {
+    void *address;
+    enum page_size pagesize;
+    bool cache_disabled;
+    bool write_through;
+    enum page_priv priv;
+    enum page_perms perms;
+    bool present;
+};
+
+struct pagetableEntry {
+    void *address;
+    bool global;
+    bool cache_disabled;
+    bool write_through;
+    enum page_priv priv;
+    enum page_perms perms;
+    bool present;
+};
+
+uint32_t make_directory_entry(struct directoryEntry de) {
+    uint32_t entry = (uint32_t)de.address & 0xFFFFF000;
+    entry |= de.pagesize << 7;
+    entry |= de.cache_disabled << 4;
+    entry |= de.write_through << 3;
+    entry |= de.priv << 2;
+    entry |= de.perms << 1;
+    entry |= de.present;
+    return entry;
 }
 
-void set_pagetable_entry(int tablenum, int entrynum, void *address, bool global, bool cache_disabled, bool write_through, enum page_priv priv, enum page_perms perms, bool present) {
-    uint32_t tentry = pagetables[tablenum][entrynum];
-    tentry = (tentry & ~0xFFFFF000) | (((uint32_t)address) & 0xFFFFF000);
-    tentry = (tentry & ~0xFFF) | (0 & 0xFFF);
-    tentry |= global << 8;
-    tentry |= cache_disabled << 6;
-    tentry |= write_through << 3;
-    tentry |= priv << 2;
-    tentry |= perms << 1;
-    tentry |= present;
-    pagetables[tablenum][entrynum] = tentry;
+uint32_t make_table_entry(struct pagetableEntry pe) {
+    uint32_t entry = ((uint32_t)pe.address) & 0xFFFFF000;
+    entry |= pe.global << 8;
+    entry |= pe.cache_disabled << 6;
+    entry |= pe.write_through << 3;
+    entry |= pe.priv << 2;
+    entry |= pe.perms << 1;
+    entry |= pe.present;
+    return entry;
 }
 
-void create_pagetable_entry(int tablenum, int entrynum, void *address, bool global, bool cache_disabled, bool write_through, enum page_priv priv, enum page_perms perms, bool present) {
-    uint32_t tentry = (uint32_t)address;
-    tentry |= global << 8;
-    tentry |= cache_disabled << 6;
-    tentry |= write_through << 3;
-    tentry |= priv << 2;
-    tentry |= perms << 1;
-    tentry |= present;
-    pagetables[tablenum][entrynum] = tentry;
+struct directoryEntry get_directory_entry(uint32_t directoryEntry) {
+    struct directoryEntry entry;
+    entry.address = (void *)(directoryEntry & 0xFFFFF000);
+    entry.present = directoryEntry & 0x1;
+    entry.perms = (page_perms)((directoryEntry >> 1) & 0x1);
+    entry.priv = (page_priv)((directoryEntry >> 2) & 0x1);
+    entry.write_through = (directoryEntry >> 3) & 0x1;
+    entry.cache_disabled = (directoryEntry >> 4) & 0x1;
+    // entry.accessed = (directoryEntry >> 5) & 0x1;
+    // entry.available = (directoryEntry >> 6) & 0x1;
+    return entry;
 }
 
-void set_directory_entry(int tablenum, void *address, enum page_size pagesize, bool cache_disabled, bool write_through, enum page_priv priv, enum page_perms perms, bool present) {
-    uint32_t direntry = page_directory[tablenum];
-    direntry = (direntry & ~0xFFFFF000) | (((uint32_t)address) & 0xFFFFF000);
-    direntry = (direntry & ~0xFFF) | (0 & 0xFFF);
-    direntry |= pagesize << 7;
-    direntry |= cache_disabled << 4;
-    direntry |= write_through << 3;
-    direntry |= priv << 2;
-    direntry |= perms << 1;
-    direntry |= present;
-    page_directory[tablenum] = direntry;
-}
-
-void delete_pagetable_entry(int tablenum, int entrynum) {
-    set_pagetable_entry(tablenum, entrynum, 0, false, true, false, SUPERVISOR, RW, false);
-}
-
-bool check_directory_entry_present(int tablenum) {
-    return page_directory[tablenum] & 0x1;
-}
-
-bool check_pagetable_entry_present(int tablenum, int entrynum) {
-    return pagetables[tablenum][entrynum] & 0x1;
+struct pagetableEntry get_table_entry(uint32_t tableEntry) {
+    struct pagetableEntry entry;
+    entry.address = (void *)(tableEntry & 0xFFFFF000);
+    entry.present = tableEntry & 0x1;
+    entry.perms = (page_perms)((tableEntry >> 1) & 0x1);
+    entry.priv = (page_priv)((tableEntry >> 2) & 0x1);
+    entry.write_through = (tableEntry >> 3) & 0x1;
+    entry.cache_disabled = (tableEntry >> 4) & 0x1;
+    // entry.accessed = (tableEntry >> 5) & 0x1;
+    // entry.dirty = (tableEntry >> 6) & 0x1;
+    // entry.PAT = (tableEntry >> 7) & 0x1;
+    entry.global = (tableEntry >> 8) & 0x1;
+    // entry.available = (tableEntry >> 9) & 0x1;
+    return entry;
 }
 
 void *paging::get_physaddr(void *virtualaddr) {
@@ -85,7 +96,6 @@ void *paging::get_physaddr(void *virtualaddr) {
 void invlpg(void *virtaddrx) {
     uint32_t virtaddr = (uint32_t)virtaddrx;
     asm volatile("invlpg (%0)" ::"r"(virtaddr) : "memory");
-    // printf("invlpg 0x%p\n", virtaddr);
 }
 
 // Both addresses have to be page-aligned!
@@ -93,8 +103,8 @@ void paging::map_page(void *physaddr, void *virtualaddr) {
     uint32_t pDirIndex = (uint32_t)virtualaddr >> 22;
     uint32_t pTableIndex = (uint32_t)virtualaddr >> 12 & 0x03FF;
 
-    bool do_invlpg = check_pagetable_entry_present(pDirIndex, pTableIndex);
-    set_pagetable_entry(pDirIndex, pTableIndex, physaddr, false, false, false, SUPERVISOR, RW, true);
+    bool do_invlpg = pagetables[pDirIndex][pTableIndex] & 0x1;
+    pagetables[pDirIndex][pTableIndex] = make_table_entry({.address = physaddr, .global = false, .cache_disabled = false, .write_through = false, .priv = SUPERVISOR, .perms = RW, .present = true});
     if (do_invlpg) {
         invlpg(virtualaddr);
     }
@@ -104,8 +114,8 @@ void unmap_page(void *virtualaddr) {
     uint32_t pDirIndex = (uint32_t)virtualaddr >> 22;
     uint32_t pTableIndex = (uint32_t)virtualaddr >> 12 & 0x03FF;
 
-    bool do_invlpg = check_pagetable_entry_present(pDirIndex, pTableIndex);
-    set_pagetable_entry(pDirIndex, pTableIndex, (void *)0, false, false, false, SUPERVISOR, RW, false);
+    bool do_invlpg = pagetables[pDirIndex][pTableIndex] & 0x1;
+    pagetables[pDirIndex][pTableIndex] = 0;
     if (do_invlpg) {
         invlpg(virtualaddr);
     }
@@ -120,14 +130,14 @@ void paging::clearPageTables(void *virtAddress, uint32_t pagecount) {
 bool paging::is_readable(const void *virtualaddr) {
     uint32_t pdindex = (uint32_t)virtualaddr >> 22;
     uint32_t ptindex = (uint32_t)virtualaddr >> 12 & 0x03FF;
-    return check_pagetable_entry_present(pdindex, ptindex);
+    return pagetables[pdindex][ptindex] & 0x1;
 }
 
 void paging::copyPhysPage(void *dest, void *src) {
     uint32_t before1 = pagetables[0][0];
     uint32_t before2 = pagetables[0][1];
-    set_pagetable_entry(0, 0, src, false, false, false, SUPERVISOR, RW, true);
-    set_pagetable_entry(0, 1, dest, false, false, false, SUPERVISOR, RW, true);
+    pagetables[0][0] = make_table_entry({.address = src, .global = false, .cache_disabled = false, .write_through = false, .priv = SUPERVISOR, .perms = RW, .present = true});
+    pagetables[0][1] = make_table_entry({.address = dest, .global = false, .cache_disabled = false, .write_through = false, .priv = SUPERVISOR, .perms = RW, .present = true});
     invlpg((void *)0);
     invlpg((void *)4096);
     memcpy((char *)4096, (char *)0, 4096);
