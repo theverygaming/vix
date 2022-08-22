b88631d209a4f2a3764da013703d5cf28c2234f4
theverygaming <clemibunge@gmail.com>
2022-08-22 04:16:43 +0200

    vector in kernel😳

---
+++ kernel/vector.h
@@ -0,0 +1,80 @@
+#include <memory_alloc/memalloc.h>
+
+template <class T> class vector {
+public:
+    vector() {
+        _capacity = 1;
+        _size = 0;
+        _pointer = (T *)memalloc::single::kmalloc(_capacity * sizeof(T));
+    }
+
+    ~vector() {
+        memalloc::single::kfree(_pointer);
+    }
+
+    T operator[](int i) {
+        if (i < _capacity) {
+            return _pointer[i];
+        }
+        return 0;
+    }
+
+    int capacity() {
+        return _capacity;
+    }
+
+    int size() {
+        return _size;
+    }
+
+    void resize(int n) {
+        _size = n;
+        reallocate(n);
+    }
+
+    void reserve(int n) {
+        if (n > _capacity) {
+            reallocate(n);
+        }
+    }
+
+    void shrink_to_fit() {
+        reallocate(_size);
+    }
+
+    void push_back(T val) {
+        assureSize(_size + 1);
+        _pointer[_size - 1] = val;
+    }
+
+    void pop_back() {
+        if (_size > 0) {
+            _size--;
+        }
+    }
+
+    void clear() {
+        _size = 0;
+        reallocate(1);
+    }
+
+private:
+    T *_pointer = 0;
+    int _capacity = 0;
+    int _size = 0;
+
+    void assureSize(int n) {
+        _size = n;
+        if (_size > _capacity) {
+            reallocate(_size);
+        }
+    }
+
+    void reallocate(int capacity) {
+        _capacity = capacity;
+        if (_capacity <= 0) {
+            _capacity = 1;
+        }
+        _pointer = (T *)memalloc::single::krealloc(_pointer, _capacity * sizeof(T));
+    }
+};
