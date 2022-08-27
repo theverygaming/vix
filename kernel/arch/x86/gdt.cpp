1f1ec047698f72fc245a35e2967c3dc9086a22f2
theverygaming <clemibunge@gmail.com>
2022-08-27 04:21:10 +0200

    stuff

--- kernel/arch/x86/gdt.cpp
+++ kernel/arch/x86/gdt.cpp
@@ -1,4 +1,6 @@
 #include "gdt.h"
+#include <../config.h>
+#include <stdlib.h>
 #include <stdint.h>
 
 typedef struct {
@@ -56,22 +58,24 @@ typedef enum {
 #define GDT_ENTRY(base, limit, access, flags)                                                                                                                                                          \
     { GDT_LIMIT_LOW(limit), GDT_BASE_LOW(base), GDT_BASE_MIDDLE(base), access, GDT_FLAGS_LIMIT_HI(limit, flags), GDT_BASE_HIGH(base) }
 
-GDTEntry g_GDT[] = {
-    // NULL descriptor
-    GDT_ENTRY(0, 0, 0, 0),
-
-    // Kernel 32-bit code segment
-    GDT_ENTRY(0, 0xFFFFF, GDT_ACCESS_PRESENT | GDT_ACCESS_RING0 | GDT_ACCESS_CODE_SEGMENT | GDT_ACCESS_CODE_READABLE, GDT_FLAG_32BIT | GDT_FLAG_GRANULARITY_4K),
-
-    // Kernel 32-bit data segment
-    GDT_ENTRY(0, 0xFFFFF, GDT_ACCESS_PRESENT | GDT_ACCESS_RING0 | GDT_ACCESS_DATA_SEGMENT | GDT_ACCESS_DATA_WRITEABLE, GDT_FLAG_32BIT | GDT_FLAG_GRANULARITY_4K),
-
-};
-
-GDTDescriptor g_GDTDescriptor = {sizeof(g_GDT) - 1, g_GDT};
-
 extern "C" void i686_GDT_Load(GDTDescriptor *descriptor, uint16_t codeSegment, uint16_t dataSegment);
 
 void gdt::i686_GDT_Initialize() {
-    i686_GDT_Load(&g_GDTDescriptor, i686_GDT_CODE_SEGMENT, i686_GDT_DATA_SEGMENT);
+    GDTEntry g_GDT[] = {
+        // NULL descriptor
+        GDT_ENTRY(0, 0, 0, 0),
+
+        // Kernel 32-bit code segment
+        GDT_ENTRY(0, 0xFFFFF, GDT_ACCESS_PRESENT | GDT_ACCESS_RING0 | GDT_ACCESS_CODE_SEGMENT | GDT_ACCESS_CODE_READABLE, GDT_FLAG_32BIT | GDT_FLAG_GRANULARITY_4K),
+
+        // Kernel 32-bit data segment
+        GDT_ENTRY(0, 0xFFFFF, GDT_ACCESS_PRESENT | GDT_ACCESS_RING0 | GDT_ACCESS_DATA_SEGMENT | GDT_ACCESS_DATA_WRITEABLE, GDT_FLAG_32BIT | GDT_FLAG_GRANULARITY_4K),
+
+    };
+
+    GDTDescriptor g_GDTDescriptor = {sizeof(g_GDT) - 1, (GDTEntry *)(KERNEL_VIRT_ADDRESS + GDT_OFFSET + sizeof(GDTDescriptor))};
+
+    memcpy((char *)(KERNEL_VIRT_ADDRESS + GDT_OFFSET), (char *)&g_GDTDescriptor, sizeof(GDTDescriptor));
+    memcpy((char *)(KERNEL_VIRT_ADDRESS + GDT_OFFSET + sizeof(GDTDescriptor)), (char *)&g_GDT, sizeof(g_GDT));
+    i686_GDT_Load((GDTDescriptor *)(KERNEL_VIRT_ADDRESS + GDT_OFFSET), i686_GDT_CODE_SEGMENT, i686_GDT_DATA_SEGMENT);
 }
