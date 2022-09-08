d8e53ddbc1b305ac2c29424bbfeb1cc0bf6350a1
theverygaming <clemibunge@gmail.com>
2022-09-08 17:23:42 +0200

    shbuild

--- kernel/scripts/shbuild.cpp
+++ kernel/scripts/shbuild.cpp
@@ -5,9 +5,8 @@
 #include <regex>
 #include <stdexcept>
 #include <string>
+#include <sys/stat.h>
 #include <vector>
-
-void processFile(std::string filename);
 
 std::map<std::string, std::vector<std::string>> variablemap;
 
@@ -33,7 +32,6 @@ void appendVariable(std::string name, std::string value) {
     if (variablemap.find(name) != variablemap.end()) {
         variablemap[name].push_back(value);
     } else {
-        // fprintf(stderr, "append: creating new variable %s\n", name.c_str());
         variablemap[name].push_back(value);
     }
 }
@@ -42,7 +40,6 @@ void appendVariable(std::string name, std::vector<std::string> value) {
     if (variablemap.find(name) != variablemap.end()) {
         variablemap[name].insert(variablemap[name].end(), value.begin(), value.end());
     } else {
-        // fprintf(stderr, "append: creating new variable %s\n", name.c_str());
         variablemap[name] = value;
     }
 }
@@ -53,7 +50,25 @@ bool isVarDefined(std::string name) {
 
 // helper functions
 bool stringStartsWith(std::string str, std::string start) {
+    if (str.length() < start.length()) {
+        return false;
+    }
     return (str.find(start) != std::string::npos) && str.find(start) == 0;
+}
+
+bool stringEndsWith(std::string str, std::string end) {
+    if (str.length() < end.length()) {
+        return false;
+    }
+    return str.find(end) == (str.length() - end.length());
+}
+
+std::string stringReplaceEnd(std::string str, std::string endBefore, std::string endAfter) {
+    if ((str.length() < endBefore.length()) || (!stringEndsWith(str, endBefore))) {
+        throw std::runtime_error("replaceEnd issue");
+    }
+    str.replace(str.length() - endBefore.length(), str.length() - 1, endAfter);
+    return str;
 }
 
 std::vector<std::string> splitString(std::string str, std::string split) {
@@ -67,51 +82,40 @@ std::vector<std::string> splitString(std::string str, std::string split) {
     return ret;
 }
 
-// parser functions
-bool parseInclude(std::string line) {
-    if (stringStartsWith(line, "include")) {
-        line.replace(0, 8, "");
-        line.erase(line.find('"'), line.find('"') + 1);
-        line.erase(line.find('"'), line.length() - 1);
-        processFile(line.c_str());
+bool doesFileNeedRebuild(std::string source, std::string object) {
+    struct stat src_buf;
+    struct stat obj_buf;
+    if (stat(source.c_str(), &src_buf) != 0) {
+        fprintf(stderr, "source file %s does not exist!\n", source.c_str());
+        throw std::runtime_error("missing source file");
+    }
+    if (stat(object.c_str(), &obj_buf) != 0) {
+        return true;
+    }
+
+    if (src_buf.st_mtime > obj_buf.st_mtime) {
         return true;
     }
     return false;
 }
 
-bool parseVar(std::string line) {
-    if (stringStartsWith(line, "varappend_tx")) {
-        line.replace(0, 13, "");
-        std::string name = line.substr(0, line.find(' '));
-        line.replace(0, line.find(' ') + 1, "");
-        appendVariable(name, splitString(line, " "));
-        return true;
-    } else if (stringStartsWith(line, "varprefix_tx")) {
-        line.replace(0, 13, "");
-        std::string name = line.substr(0, line.find(' '));
-        line.replace(0, line.find(' ') + 1, "");
-        std::vector<std::string> &var = getVariable(name);
-        for (int i = 0; i < var.size(); i++) {
-            var[i].insert(0, line.c_str());
-        }
-        return true;
-    } else if (stringStartsWith(line, "varappend")) {
-        char buf_dst[400];
-        char buf_src[400];
-        if (line.length() > 400) {
-            throw std::runtime_error("insane line length");
-        }
-        if (sscanf(line.c_str(), "varappend %s %s", buf_dst, buf_src) != 2) {
-            throw std::runtime_error("syntax error");
-        }
-        appendVariable(buf_dst, getVariable(buf_src));
-        return true;
+inline void runcmd(std::string cmd) {
+    if (system(cmd.c_str())) {
+        fprintf(stderr, "failed: %s\n", cmd.c_str());
+        throw std::runtime_error("command failed");
     }
-    return false;
 }
 
-std::string lastFolder = "";
+std::string expandStrVector(std::vector<std::string> in) {
+    std::string ret;
+    for (int i = 0; i < in.size(); i++) {
+        ret.append(in[i] + " ");
+    }
+    return ret;
+}
+
 void processFile(std::string filename) {
+    static std::string lastFolder = "";
     std::string savedFolder = lastFolder;
     filename = lastFolder + filename;
     std::ifstream infile(filename);
@@ -122,7 +126,7 @@ void processFile(std::string filename) {
 
     // get folder
     if (filename.find_last_of("/") != std::string::npos) {
-        lastFolder = lastFolder + filename.substr(0, filename.find_last_of("/")) + "/";
+        lastFolder = filename.substr(0, filename.find_last_of("/")) + "/";
     }
 
     // set CURRENTPATH variable
@@ -136,23 +140,15 @@ void processFile(std::string filename) {
         }
     }
 
-    int currentLine = 0;
+    size_t currentLine = 0;
     while (currentLine < lines.size()) {
         std::string line = lines[currentLine];
 
-        // fprintf(stderr, "%s:%d -> %s\n", filename.c_str(), currentLine+1, line.c_str());
         //  remove comments
         if (line.find("//") != std::string::npos) {
             line.erase(line.find("//"), line.length());
         }
         if (line.length() == 0) {
-            currentLine++;
-            continue;
-        }
-        // fprintf(stderr, "%s:%d -> %s\n", filename.c_str(), currentLine+1, line.c_str());
-
-        // is this an include?
-        if (parseInclude(line)) {
             currentLine++;
             continue;
         }
@@ -162,7 +158,7 @@ void processFile(std::string filename) {
             std::smatch match;
             if (std::regex_search(line, match, std::regex("VARINSERT\\((.*?)\\)"))) {
                 std::string all = "";
-                for (int i = 0; i < getVariable(match.str(1)).size(); i++) {
+                for (size_t i = 0; i < getVariable(match.str(1)).size(); i++) {
                     all = all + getVariable(match.str(1))[i] + " ";
                 }
                 if (all.length() >= 1) {
@@ -171,18 +167,27 @@ void processFile(std::string filename) {
                 line = std::regex_replace(line, std::regex("VARINSERT\\((.*?)\\)"), all);
             }
         }
-        // fprintf(stderr, "%s:%d -> %s\n", filename.c_str(), currentLine+1, line.c_str());
+
+        // is this an include?
+        if (stringStartsWith(line, "include ")) {
+            line.replace(0, 8, "");
+            line.erase(line.find('"'), line.find('"') + 1);
+            line.erase(line.find('"'), line.length() - 1);
+            processFile(line.c_str());
+            currentLine++;
+            continue;
+        }
 
         // is this an ifdef?
-        if (stringStartsWith(line, "ifvardef")) {
+        if (stringStartsWith(line, "ifvardef ")) {
             int lineSkipCount;
             char buf[200];
             if (line.length() > 200) {
-                fprintf(stderr, "insane line length at %s:%d -> %s\n", filename.c_str(), currentLine + 1, line.c_str());
+                fprintf(stderr, "insane line length at %s:%zu -> %s\n", filename.c_str(), currentLine + 1, line.c_str());
                 throw std::runtime_error("insane line length");
             }
             if (sscanf(line.c_str(), "ifvardef %s %d", buf, &lineSkipCount) != 2) {
-                fprintf(stderr, "syntax error at %s:%d -> %s\n", filename.c_str(), currentLine + 1, line.c_str());
+                fprintf(stderr, "syntax error at %s:%zu -> %s\n", filename.c_str(), currentLine + 1, line.c_str());
                 throw std::runtime_error("syntax error");
             }
             if (!isVarDefined(buf)) {
@@ -192,73 +197,30 @@ void processFile(std::string filename) {
             continue;
         }
 
-        // is this an ifvarisnot?
-        if (stringStartsWith(line, "ifvarisnot")) {
-            int lineSkipCount;
-            char buf[200];
-            char value[200];
-            if (line.length() > 200) {
-                fprintf(stderr, "insane line length at %s:%d -> %s\n", filename.c_str(), currentLine + 1, line.c_str());
-                throw std::runtime_error("insane line length");
-            }
-            if (sscanf(line.c_str(), "ifvarisnot %s %s %d", buf, value, &lineSkipCount) != 3) {
-                fprintf(stderr, "syntax error at %s:%d -> %s\n", filename.c_str(), currentLine + 1, line.c_str());
-                throw std::runtime_error("syntax error");
-            }
-            if (getVariable(buf).size() > 0) {
-                if (!strcmp(getVariable(buf)[0].c_str(), value)) {
-                    currentLine += lineSkipCount;
-                }
-            }
-            currentLine++;
-            continue;
-        }
-
-        if (stringStartsWith(line, "arraylength")) {
-            line.replace(0, 12, "");
-            std::string srcarray = line.substr(0, line.find(' '));
-            line.replace(0, line.find(' ') + 1, "");
-            std::string dstvar = line;
-            printf("src: %s dst: %s\n", srcarray.c_str(), dstvar.c_str());
-            setVariable(dstvar, std::to_string(getVariable(srcarray).size()));
-            currentLine++;
-            continue;
-        }
-
-        if (stringStartsWith(line, "getarrindex")) {
-            line.replace(0, 12, "");
-            std::string array = line.substr(0, line.find(' '));
-            line.replace(0, line.find(' ') + 1, "");
-            std::string index = getVariable(line.substr(0, line.find(' ')))[0];
-            line.replace(0, line.find(' ') + 1, "");
-            std::string dstvar = line;
-            if (getVariable(array).size() > std::stoi(index) - 1) {
-                setVariable(dstvar, getVariable(array)[std::stoi(index) - 1]);
-            } else {
-                fprintf(stderr, "array out of range at %s:%d -> %s\n", filename.c_str(), currentLine + 1, line.c_str());
-                throw std::runtime_error("array out of range");
-            }
-            currentLine++;
-            continue;
-        }
-
         // is this some variable operation?
-        if (parseVar(line)) {
-            currentLine++;
-            continue;
-        }
-
-        if (stringStartsWith(line, "subtract")) {
-            line.replace(0, 9, "");
-            std::string dstvar = line.substr(0, line.find(' '));
+        if (stringStartsWith(line, "varappend_tx_prefix ")) {
+            line.replace(0, 20, "");
+            std::string name = line.substr(0, line.find(' '));
             line.replace(0, line.find(' ') + 1, "");
-            std::string amount = line;
-            setVariable(dstvar, std::to_string(std::stoi(getVariable(dstvar)[0]) - std::stoi(amount)));
+            std::string prefix = line.substr(0, line.find(' '));
+            line.replace(0, line.find(' ') + 1, "");
+            std::vector<std::string> toAppend = splitString(line, " ");
+            for (size_t i = 0; i < toAppend.size(); i++) {
+                toAppend[i].insert(0, prefix);
+            }
+            appendVariable(name, toAppend);
+            currentLine++;
+            continue;
+        } else if (stringStartsWith(line, "varset_tx ")) {
+            line.replace(0, 10, "");
+            std::string name = line.substr(0, line.find(' '));
+            line.replace(0, line.find(' ') + 1, "");
+            setVariable(name, line);
             currentLine++;
             continue;
         }
 
-        if (stringStartsWith(line, "shellcmd")) {
+        if (stringStartsWith(line, "shellcmd ")) {
             line.replace(0, 9, "");
             std::string cmd = line;
             system(cmd.c_str());
@@ -266,24 +228,42 @@ void processFile(std::string filename) {
             continue;
         }
 
-        if (stringStartsWith(line, "print")) {
-            line.replace(0, 6, "");
-            printf("%s\n", line.c_str());
-            currentLine++;
-            continue;
-        }
-
-        if (stringStartsWith(line, "jump")) {
-            sscanf(line.c_str(), "jump %d", &currentLine);
-            currentLine--; // jump line number starts at 1
-            continue;
-        }
-
-        fprintf(stderr, "%s:%d -> couldn't parse\n", filename.c_str(), currentLine + 1);
+        fprintf(stderr, "%s:%zu -> couldn't parse %s\n", filename.c_str(), currentLine + 1, line.c_str());
         throw std::runtime_error("syntax error");
     }
 
     lastFolder = savedFolder;
+    setVariable("CURRENTPATH", lastFolder);
+}
+
+void build_cpp_gcc(std::string cxx, std::string cxxflags, std::string inputfile, std::string outputfile) {
+    std::string command = cxx + " " + cxxflags + " -c " + inputfile + " -o " + outputfile;
+    printf("%s %s\n", cxx.c_str(), inputfile.c_str());
+    runcmd(command);
+}
+
+void build_asm_nasm(std::string nasm, std::string nflags, std::string inputfile, std::string outputfile) {
+    std::string command = nasm + " " + nflags + " " + inputfile + " -o " + outputfile;
+    printf("%s %s\n", nasm.c_str(), inputfile.c_str());
+    runcmd(command);
+}
+
+void partial_link(std::string ld, std::string ldflags, std::string linkerscript, std::string inputfiles, std::string outputobject) {
+    std::string command = ld + " " + ldflags + " -r -T " + linkerscript + " " + inputfiles + " -o " + outputobject;
+    printf("%s %s\n", ld.c_str(), inputfiles.c_str());
+    runcmd(command);
+}
+
+void final_link(std::string ld, std::string ldflags, std::string linkerscript, std::string inputfiles, std::string outputobject) {
+    std::string command = ld + " " + ldflags + " -T " + linkerscript + " " + inputfiles + " -o " + outputobject;
+    printf("%s %s\n", ld.c_str(), inputfiles.c_str());
+    runcmd(command);
+}
+
+void final_objcopy(std::string objcopy, std::string objcopyflags, std::string input, std::string output) {
+    std::string command = objcopy + " " + objcopyflags + " " + input + " " + output;
+    printf("%s %s -> %s\n", objcopy.c_str(), input.c_str(), output.c_str());
+    runcmd(command);
 }
 
 int main(int argc, char *argv[]) {
@@ -292,12 +272,36 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    fprintf(stderr, "shbuild: processing file %s\n", argv[1]);
     processFile(argv[1]);
-    std::vector<std::string> test = getVariable("objs");
-    for (int i = 0; i < test.size(); i++) {
-        // printf("%s\n", test[i].c_str());
+    std::vector<std::string> objs = getVariable("objs");
+    for (size_t i = 0; i < objs.size(); i++) {
+        if (stringEndsWith(objs[i], ".ocpp")) {
+            std::string sourcefile = stringReplaceEnd(objs[i], ".ocpp", ".cpp");
+            if (doesFileNeedRebuild(sourcefile, objs[i])) {
+                build_cpp_gcc(
+                    "g++",
+                    "-Iinclude -O3 -nostdlib -m32 -march=i386 -fno-pie -mno-red-zone -fno-stack-protector -ffreestanding -fno-exceptions -Wall -Wextra -Wno-pointer-arith -Wno-unused-parameter",
+                    sourcefile,
+                    objs[i]);
+            } else {
+                printf("%s doesn't need to rebuild\n", objs[i].c_str());
+            }
+            continue;
+        }
+        if (stringEndsWith(objs[i], ".oasm")) {
+            std::string sourcefile = stringReplaceEnd(objs[i], ".oasm", ".asm");
+            if (doesFileNeedRebuild(sourcefile, objs[i])) {
+                build_asm_nasm("nasm", "-f elf", sourcefile, objs[i]);
+            } else {
+                printf("%s doesn't need to rebuild\n", objs[i].c_str());
+            }
+            continue;
+        }
+        printf("unknown file type -> %s\n", objs[i].c_str());
+        return 1;
     }
+    final_link("ld", "-nostdlib -m elf_i386 -z noexecstack", "linker.ld", expandStrVector(objs), "kernel.o");
+    final_objcopy("objcopy", "-O binary", "kernel.o", "kernel.bin");
 
     return 0;
 }
