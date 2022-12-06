35c8b9fb2a24cab687e3bdf85b924b8bc4635233
theverygaming <clemibunge@gmail.com>
2022-12-06 07:08:17 +0100

    fix github actions symbol table

--- kernel/build.sh
+++ kernel/build.sh
@@ -4,7 +4,7 @@ set -e # return if command failed
 
 scripts/shbuild shbuild
 if [ "${MAKE_ARCH}" == "x86" ]; then # build symbol table and link it
-    nm --format=bsd --quiet -n kernel.o | python3 scripts/gensyms.py > symtab.s
+    nm --format=bsd -n kernel.o | python3 scripts/gensyms.py > symtab.s
     g++ -O3 -Iinclude -nostdlib -m32 -march=i386 -fno-pie -mno-red-zone -fno-stack-protector -ffreestanding -fno-exceptions -Wall -Wextra -Wno-unused-parameter -c symtab.s -o symtab.os
     ld -nostdlib -m elf_i386 -z noexecstack -T arch/x86/linker.ld kernel_partital.o symtab.os -o kernel.o
     objcopy -O binary kernel.o kernel.bin
