316009e6c95c5949b54cbb62fc95856d93cfb77a
theverygaming <theverygaming@users.noreply.github.com>
2024-09-30 12:51:10 +0200

    aarch64 nix stuff

--- nix-shells/aarch64.nix
+++ nix-shells/aarch64.nix
@@ -11,7 +11,8 @@ pkgs.stdenv.mkDerivation {
   ] ++ common.commonPkgs ++ common.limine ++ common.fatTools;
 
   shellHook = common.shellHook + ''
+    export VIX_QEMU_UEFI_BIOS_PATH="${pkgs.qemu}/share/qemu/edk2-aarch64-code.fd"
     alias vix-build='make MAKE_ARCH=aarch64 CROSS_COMPILE=aarch64-none-elf- -j$NIX_BUILD_CORES && make bootimg-aarch64'
-    alias vix-run='qemu-system-aarch64 -m 1024 -bios "${pkgs.qemu}/share/qemu/edk2-aarch64-code.fd" -cpu cortex-a53 -smp 4 -machine virt -device ramfb -device qemu-xhci -device usb-kbd -hda vix_uefi.img'
+    alias vix-run='qemu-system-aarch64 -m 1024 -bios "$VIX_QEMU_UEFI_BIOS_PATH" -cpu cortex-a53 -smp 4 -machine virt -device ramfb -device qemu-xhci -device usb-kbd -hda vix_uefi.img'
   '';
 }
