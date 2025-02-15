eb95f59834d7d1429d6e56cfd0c811271d0f6f12
theverygaming <theverygaming@users.noreply.github.com>
2025-02-15 15:33:43 +0100

    better panics from rust

--- kernel/rust/libkernel/bindings.h
+++ kernel/rust/libkernel/bindings.h
@@ -1,2 +1,3 @@
-#include <vix/mm/kheap.h>
 #include <vix/kprintf.h>
+#include <vix/mm/kheap.h>
+#include <vix/panic.h>
--- kernel/rust/libkernel/panic.rs
+++ kernel/rust/libkernel/panic.rs
@@ -1,7 +1,23 @@
 use core::panic::PanicInfo;
+use alloc::ffi::CString;
+use alloc::format;
+
+use crate::bindings::{
+    kernel_panic,
+};
 
 #[panic_handler]
 fn panic(panic_info: &PanicInfo<'_>) -> ! {
-    crate::klog!(crate::klog::KP_EMERG, "Rust panic: {}", panic_info.message());
-    loop {} // FIXME: log
+    match (
+        CString::new("Rust panic: %s\n"),
+        CString::new(format!("{}", panic_info.message())),
+    ) {
+        (Ok(fmt), Ok(info)) => unsafe {
+            kernel_panic(fmt.as_ptr(), info.as_ptr());
+        },
+        _ => unsafe {
+            // HACK: we manually add a silly null terminator because rust strings are not null-terminated
+            kernel_panic("Massive issue: rust panic failed!\n\0".as_bytes().as_ptr() as *const core::ffi::c_char);
+        },
+    }
 }
