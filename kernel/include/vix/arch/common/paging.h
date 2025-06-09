e2d6a2fd05063d59b04861da08d78124a62345bf
theverygaming <theverygaming@users.noreply.github.com>
2025-06-10 00:05:12 +0200

    add arch::vmm::FLAGS_ACCESSED

--- kernel/include/vix/arch/common/paging.h
+++ kernel/include/vix/arch/common/paging.h
@@ -7,26 +7,30 @@
 namespace arch::vmm {
     // if set the page is present in physical memory (not swapped out)
     inline const unsigned int FLAGS_PRESENT = (0x1 << 0);
+    // if set the page has been accessed (written, read etc.)
+    inline const unsigned int FLAGS_ACCESSED = (0x1 << 1);
     // if set the page has been written to
-    inline const unsigned int FLAGS_DIRTY = (0x1 << 1);
+    inline const unsigned int FLAGS_DIRTY = (0x1 << 2);
     // if set page will not be cached
-    inline const unsigned int FLAGS_CACHE_DISABLE = (0x1 << 2);
+    inline const unsigned int FLAGS_CACHE_DISABLE = (0x1 << 3);
     // if set write-back caching will be enabled for this page
-    inline const unsigned int FLAGS_WRITE_BACK = (0x1 << 3);
+    inline const unsigned int FLAGS_WRITE_BACK = (0x1 << 4);
     // if set write-through caching will be enabled for this page
-    inline const unsigned int FLAGS_WRITE_THROUGH = (0x1 << 4);
+    inline const unsigned int FLAGS_WRITE_THROUGH = (0x1 << 5);
     // if set write-combined caching will be enabled for this page
-    inline const unsigned int FLAGS_WRITE_COMBINING = (0x1 << 5);
+    inline const unsigned int FLAGS_WRITE_COMBINING = (0x1 << 6);
     // if set the page can be accessed from usermode
-    inline const unsigned int FLAGS_USER = (0x1 << 6);
+    inline const unsigned int FLAGS_USER = (0x1 << 7);
     // if set the page will be read only to usermode and maybe even kernel mode
-    inline const unsigned int FLAGS_READ_ONLY = (0x1 << 7);
+    inline const unsigned int FLAGS_READ_ONLY = (0x1 << 8);
     // if set the page cannot be executed as code
-    inline const unsigned int FLAGS_NO_EXECUTE = (0x1 << 8);
+    inline const unsigned int FLAGS_NO_EXECUTE = (0x1 << 9);
     // if set the page will not be flushed when a syscall from usermode happens (on x86 this is the global bit) -- this should never be enabled for user pages, bad idea
-    inline const unsigned int FLAGS_NO_FLUSH_ON_PRIV_CHANGE = (0x1 << 9);
-    // flag that never gets touched by the CPU, usable for the OS for arbitrary purposes
-    inline const unsigned int FLAGS_OS_FLAG_1 = (0x1 << 10);
+    inline const unsigned int FLAGS_NO_FLUSH_ON_PRIV_CHANGE = (0x1 << 10);
+    // flag that never gets touched by the MMU, usable for the OS for arbitrary purposes
+    inline const unsigned int FLAGS_OS_FLAG_1 = (0x1 << 11);
+    // flag that never gets touched by the MMU, usable for the OS for arbitrary purposes
+    inline const unsigned int FLAGS_OS_FLAG_2 = (0x1 << 12);
 
     /**
      * gets flags and physical address for page
