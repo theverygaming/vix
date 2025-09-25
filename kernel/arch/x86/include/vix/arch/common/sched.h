0fa26b50a544385e9b1e0a66ef207fbc3df699de
theverygaming <theverygaming@users.noreply.github.com>
2025-09-26 01:15:54 +0200

    x86: fix x86_64 build

--- kernel/arch/x86/include/vix/arch/common/sched.h
+++ kernel/arch/x86/include/vix/arch/common/sched.h
@@ -4,7 +4,9 @@
 
 namespace sched {
     struct arch_task {
+#ifdef CONFIG_ARCH_HAS_PAGING
         arch::vmm::pt_t pt;
+#endif
         void *kernel_stack_bottom;
         void *kernel_stack_top;
         bool is_ring_3;
