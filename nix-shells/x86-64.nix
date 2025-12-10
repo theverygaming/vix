066dd31812b9538c57c515411a1472409ae7baf0
theverygaming <theverygaming@users.noreply.github.com>
2025-12-10 11:32:17 +0100

    x86_64 gdb command

--- nix-shells/x86-64.nix
+++ nix-shells/x86-64.nix
@@ -13,5 +13,7 @@ pkgs.stdenv.mkDerivation {
   shellHook = common.shellHook + ''
     alias vix-build='make MAKE_ARCH=x86 CROSS_COMPILE=x86_64-elf- -j$NIX_BUILD_CORES && make bootimg-x86-64 MAKE_ARCH=x86'
     alias vix-run='qemu-system-x86_64 -hda vix_uefi.img -m 512'
+    alias vix-debug='vix-run -s -S'
+    alias vix-debugger='gdb kernel/kernel.o -ex "set architecture i386:x86-64" -ex "target remote localhost:1234"'
   '';
 }
