5e22865f71b9d847ff39c2ead8706fda76ae00c1
theverygaming <theverygaming@users.noreply.github.com>
2026-04-09 11:30:09 +0200

    allow multiple in-tree kernel modules

--- kernel/include/vix/module.h
+++ kernel/include/vix/module.h
@@ -1,9 +1,11 @@
 #pragma once
 
-#define MODULE_AUTHOR(value)      __attribute__((section(".moduleinfo"), used)) const char *__MODULE_AUTHOR = value
-#define MODULE_DESCRIPTION(value) __attribute__((section(".moduleinfo"), used)) const char *__MODULE_DESCRIPTION = value
-#define MODULE_VERSION(value)     __attribute__((section(".moduleinfo"), used)) const char *__MODULE_VERSION = value
+// HACK: all of these are weak to avoid multiple definition error when having multiple modules in-tree.. We need a new solution for this
+
+#define MODULE_AUTHOR(value)      __attribute__((section(".moduleinfo"), used, weak)) const char *__MODULE_AUTHOR = value
+#define MODULE_DESCRIPTION(value) __attribute__((section(".moduleinfo"), used, weak)) const char *__MODULE_DESCRIPTION = value
+#define MODULE_VERSION(value)     __attribute__((section(".moduleinfo"), used, weak)) const char *__MODULE_VERSION = value
 // clang-format off
-#define MODULE_INIT(func) extern "C" int __MODULE_INIT() { return func(); }
-#define MODULE_EXIT(func) extern "C" void __MODULE_EXIT() { func(); }
+#define MODULE_INIT(func) extern "C" int __attribute__((weak)) __MODULE_INIT() { return func(); }
+#define MODULE_EXIT(func) extern "C" void __attribute__((weak)) __MODULE_EXIT() { func(); }
 // clang-format on
