436945ae4bfd7846a742f1d48a9619c90830d710
theverygaming <theverygaming@users.noreply.github.com>
2026-04-09 09:43:04 +0200

    improved IPv4 parsing

--- kernel/net/stack_rs/ipv4.rs
+++ kernel/net/stack_rs/ipv4.rs
@@ -31,33 +31,87 @@ pub struct IPv4Header {
 
 impl fmt::Debug for IPv4Header {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "IPv4Header {{ source: {:?}, destination: {:?} }}", self.source, self.destination)
+        write!(
+            f,
+            "IPv4Header {{ diff_serv: 0x{:x} ID: 0x{:x} flags: 0x{:x} fragment offset: 0x{:x} TTL: {} protocol: {} source: {:?}, destination: {:?} }}",
+            self.diff_serv,
+            self.identification,
+            self.flags,
+            self.fragment_offset,
+            self.ttl,
+            self.protocol,
+            self.source,
+            self.destination,
+        )
     }
 }
 
 impl IPv4Header {
     pub fn deserialize(data: &[u8]) -> Option<(Self, usize)> {
-        if data.len() <= 20 {
+        if data.len() < 20 {
             return None;
         }
 
-        // TODO: checksum!
-
+        let version = (data[0] >> 4) & 0xF;
+        if version != 4 {
+            return None;
+        }
+        let header_len = data[0] & 0xF;
+        let header_bytes = header_len as u16 * 4;
+        if header_bytes < 20 {
+            return None;
+        }
+        let diff_serv = data[1];
+        let total_len = u16::from_be_bytes(data[2..4].try_into().unwrap());
+        if total_len < header_bytes {
+            return None;
+        }
+        let identification = u16::from_be_bytes(data[4..6].try_into().unwrap());
+        let flags = data[6] & 0x7;
+        let fragment_offset = u16::from_be_bytes(data[6..8].try_into().unwrap()) & 0x1FFF;
+        let ttl = data[8];
+        let protocol = data[9];
+        let checksum = u16::from_be_bytes(data[10..12].try_into().unwrap());
         let source = IPv4Address(data[12..16].try_into().unwrap());
         let destination = IPv4Address(data[16..20].try_into().unwrap());
+        // TODO: handle options?
+
+        let checksum_computed = Self::compute_checksum(data, header_bytes);
+        if checksum != checksum_computed {
+            return None;
+        }
 
         Some((
             Self {
-                diff_serv: 0,
-                identification: 0,
-                flags: 0,
-                fragment_offset: 0,
-                ttl: 0,
-                protocol: 0,
+                diff_serv: diff_serv,
+                identification: identification,
+                flags: flags,
+                fragment_offset: fragment_offset,
+                ttl: ttl,
+                protocol: protocol,
                 source: source,
                 destination: destination,
             },
-            20, // data offset
+            header_bytes as usize, // data offset
         ))
+    }
+
+    fn compute_checksum(data: &[u8], header_bytes: u16) -> u16 {
+        // u32 sum will easily fit 15*2*0xFFFF without overflowing
+        let mut sum: u32 = 0;
+        for i in (0..header_bytes as usize).step_by(2) {
+            // the checksum field itself is always zero during the checksum computation
+            let val = if i != 10 {
+                u16::from_be_bytes(data[i..i+2].try_into().unwrap())
+            } else {
+                0
+            };
+            sum += val as u32;
+        }
+        sum = ((sum >> 16) & 0xF) + (sum & 0xFFFF);
+        sum = ((sum >> 16) & 0xF) + (sum & 0xFFFF);
+        let mut checksum: u16 = sum.try_into().unwrap();
+        checksum = !checksum;
+        return checksum;
     }
 }
