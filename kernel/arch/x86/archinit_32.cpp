735aecd2741c8f5756215bd94302c7e23fb6fa0b
theverygaming <18639279+theverygaming@users.noreply.github.com>
2026-08-29 05:29:26 +0200

    fix crash on x86 due to stack getting zeroed
    The stack lives in the .bss section, and we zeroed that *inside* a function that stores the miltiboot2 info pointer on the stack :c

--- kernel/arch/x86/archinit_32.cpp
+++ kernel/arch/x86/archinit_32.cpp
@@ -101,17 +101,11 @@ static void kernelinit(void *multiboot2_info_ptr) {
     kernelstart();
 }
 
-extern "C" uint8_t _bss_start;
-extern "C" uint8_t _bss_end;
-
 extern "C" void __attribute__((section(".entry"))) _kentry(uint32_t multiboot2_info_ptr) {
     size_t sp;
     asm volatile("mov %%esp, %0" : "=a"(sp) :);
     if (sp < CONFIG_KERNEL_HIGHER_HALF) {
         return;
-    }
-    for (uint8_t *addr = &_bss_start; addr < &_bss_end; addr++) {
-        *addr = 0;
     }
     kernelinit((void *)multiboot2_info_ptr);
     while (true) {}
