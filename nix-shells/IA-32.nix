08ec5a89eb220f9fb807270899f8174ff8f26e77
theverygaming <18639279+theverygaming@users.noreply.github.com>
2026-08-29 05:30:26 +0200

    fix IA-32 debugging

--- nix-shells/IA-32.nix
+++ nix-shells/IA-32.nix
@@ -25,8 +25,8 @@ pkgs.stdenv.mkDerivation {
 
   shellHook = common.shellHook + ''
     alias vix-build='make MAKE_ARCH=x86 CROSS_COMPILE=i686-elf- -j$NIX_BUILD_CORES && make bootimg-x86-32 MAKE_ARCH=x86 CROSS_COMPILE=i686-elf-'
-    alias vix-run='qemu-system-x86_64 -cdrom vix.iso -m 512'
+    alias vix-run='qemu-system-i386 -cpu pentium3 -cdrom vix.iso -m 512'
     alias vix-debug='vix-run -s -S'
-    alias vix-debugger='gdb kernel/kernel.o -ex "set architecture i386:x86-64" -ex "target remote localhost:1234"'
+    alias vix-debugger='gdb kernel/kernel.o -ex "set architecture i386" -ex "target remote localhost:1234"'
   '';
 }
