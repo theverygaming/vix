f7ebe4e221e16ad293cb08b073c6dc256c696e0d
theverygaming <18639279+theverygaming@users.noreply.github.com>
2026-08-29 02:24:25 +0200

    fix esp32 build

--- kernel/arch/xtensa/sched.cpp
+++ kernel/arch/xtensa/sched.cpp
@@ -10,9 +10,9 @@ static void procret() {
 
 void sched::arch_init_thread(struct sched::thread *proc, void (*func)()) {
     uint32_t *stack = (uint32_t *)((uint8_t *)mm::kmalloc(256) + 256);
-    proc->ctx.sp = (uint32_t)stack;
-    proc->ctx.ra =
-        ((uint32_t)func & (~(0b11ul << 30))) | 2 << 30; // The two MSB's of the return address are the callx instruction used - in this case call8
+    struct arch::ctx *ctx = (struct arch::ctx *)stack;
+    ctx->ra = ((uint32_t)func & (~(0b11ul << 30))) | 2 << 30; // The two MSB's of the return address are the callx instruction used - in this case call8
+    proc->ctx = ctx;
 }
 
 extern "C" void sched_switch(struct arch::ctx **old, struct arch::ctx *_new, struct sched::thread *prev, struct sched::thread *next) {
