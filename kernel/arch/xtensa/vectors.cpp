b8bc8f2d6dcdc9ede3a57094a960541b303bcd7e
theverygaming <18639279+theverygaming@users.noreply.github.com>
2026-08-31 01:46:07 +0200

    xtensa: clean up forgotten debug prints

--- kernel/arch/xtensa/vectors.cpp
+++ kernel/arch/xtensa/vectors.cpp
@@ -2,17 +2,10 @@
 #include <vix/arch/vectors.h>
 #include <vix/kprintf.h>
 
-//extern "C" void xtensa_setup_vectors();
-extern "C" uint32_t xtensa_setup_vectors();
+extern "C" void xtensa_setup_vectors();
 
 void arch::init_vectors() {
-    uint32_t vecbase;
-    asm volatile("rsr.vecbase %0" : "=r"(vecbase));
-    kprintf(KP_INFO, "VECBASE: 0x%p\n", vecbase);
-    //xtensa_setup_vectors();
-    kprintf(KP_INFO, "VECBASE set: 0x%p\n", xtensa_setup_vectors());
-    asm volatile("rsr.vecbase %0" : "=r"(vecbase));
-    kprintf(KP_INFO, "VECBASE: 0x%p\n", vecbase);
+    xtensa_setup_vectors();
 }
 
 extern "C" void xtensa_vector_debug(uint32_t epc1, uint32_t epc2, uint32_t epc3, uint32_t exccause, uint32_t debugcause, uint32_t interrupt) {
