44dfb618fac11de0d410f3ff2d63b84aa49c2be7
theverygaming <18639279+theverygaming@users.noreply.github.com>
2026-08-31 00:45:37 +0200

    xtensa: add skeleton to allow support for more hardware

--- kernel/arch/xtensa/archlink.sh
+++ kernel/arch/xtensa/archlink.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env bash
 set -eu
 
-${INT_LD} ${INT_LDFLAGS} -T arch/xtensa/linker.ld kernel_partial.o -o kernel.o
+source .config
+
+if [ "${CONFIG_XTENSA_TARGET_ESP32:-}" == "y" ]; then
+    ldscript="arch/xtensa/linker_esp32.ld"
+fi
+
+${INT_LD} ${INT_LDFLAGS} -T "${ldscript}" kernel_partial.o -o kernel.o
 ${INT_NM} --format=bsd -n kernel.o | python3 scripts/gensyms.py .long > symtab.S
 ${INT_CXX} ${INT_CXXFLAGS} -c symtab.S -o symtab.o
-${INT_LD} ${INT_LDFLAGS} -T arch/xtensa/linker.ld kernel_partial.o symtab.o -o kernel.o
+${INT_LD} ${INT_LDFLAGS} -T "${ldscript}" kernel_partial.o symtab.o -o kernel.o
 
 ${INT_NM} --format=bsd -n kernel.o | python3 scripts/gensyms.py .long > symtab.S
 ${INT_CXX} ${INT_CXXFLAGS} -c symtab.S -o symtab.o
-${INT_LD} ${INT_LDFLAGS} -T arch/xtensa/linker.ld kernel_partial.o symtab.o -o kernel.o
+${INT_LD} ${INT_LDFLAGS} -T "${ldscript}" kernel_partial.o symtab.o -o kernel.o
