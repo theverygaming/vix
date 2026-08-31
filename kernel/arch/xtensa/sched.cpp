db60ac798b89e9657892bc6e39e5f3768547cf96
theverygaming <18639279+theverygaming@users.noreply.github.com>
2026-08-31 13:08:23 +0200

    fix esp32 build

--- kernel/arch/xtensa/sched.cpp
+++ kernel/arch/xtensa/sched.cpp
@@ -3,16 +3,19 @@
 #include <vix/sched.h>
 #include <vix/types.h>
 #include <vix/sched.h>
+#include <vix/config.h>
 
 static void procret() {
     KERNEL_PANIC("returned from process");
 }
 
 void sched::arch_init_thread(struct sched::thread *proc, void (*func)()) {
+#ifdef CONFIG_XTENSA_TARGET_ESP8266
     uint32_t *stack_bottom = (uint32_t *)mm::kmalloc_aligned(1024, 16);
     uint32_t *stack_top = (uint32_t *)((uint8_t *)stack_bottom + 1024);
     stack_top -= 32 / 4; // stack must always be 16-byte aligned
     struct arch::ctx *ctx = (struct arch::ctx *)stack_top;
     ctx->a0 = (uint32_t)func;
     proc->ctx = ctx;
+#endif
 }
