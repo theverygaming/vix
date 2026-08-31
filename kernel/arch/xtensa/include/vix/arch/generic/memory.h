5293c349aee9d04336d85fef6f569f3e781d896c
theverygaming <18639279+theverygaming@users.noreply.github.com>
2026-08-31 03:53:01 +0200

    set ARCH_ALIGNMENT_REQUIRED correctly on xtensa

--- kernel/arch/xtensa/include/vix/arch/generic/memory.h
+++ kernel/arch/xtensa/include/vix/arch/generic/memory.h
@@ -3,8 +3,4 @@
 #include <vix/types.h>
 
 /* alignment for architectures that require it */
-#define ARCH_ALIGNMENT_REQUIRED 8
-
-/* needed for memory allocators -- VIRTUAL addresses -- must be aligned to CONFIG_ARCH_PAGE_SIZE */
-#define ARCH_KERNEL_HEAP_START 0x190000
-#define ARCH_KERNEL_HEAP_END   (ARCH_KERNEL_HEAP_START + 0xFFFFFF)
+#define ARCH_ALIGNMENT_REQUIRED 4
